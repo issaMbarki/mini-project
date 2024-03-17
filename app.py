4a94d64fe89d09dd9c62df35c2c446b1ebf2bfdf
issaMbarki <49issam@gmail.com>
2024-03-17 03:38:20 +0000

    Update timer logic and UI.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, jsonify, redirect, url_for, request
 import time
 import threading
-
+import logging
 from modules.hadnle_form import handle_form_data
 
 app = Flask(__name__)
 
+# Disable the default Flask logger
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
 
 
 class Timer:
@@ -22,24 +25,117 @@ class Timer:
         self.temps_travail_par_exercice = temps_travail_par_exercice
         self.temps_repos_exercices = temps_repos_exercices
         self.temps_repos_series = temps_repos_series
-        #####
+        # Additional variables used in the update_timer function
         self.temps_total = (
             (temps_travail_par_exercice + temps_repos_exercices) * nombre_exercices
         ) * nombre_series + (nombre_series - 1) * temps_repos_series
-
-        self.remaining_time = self.temps_total + 1
+        self.remaining_time = self.calc_temps_total()
+        self.current_serie = 1
+        self.current_exercise = 1
+        self.worked_time = 0
+        self.exercise_break_countdown = temps_repos_exercices
+        self.serie_break_countdown = temps_repos_series
+        self.is_in_exercise_break = False
+        self.is_in_serie_break = False
         self.is_running = False
         self.timer_thread = None
         self.notification_sound = "notification_sound.mp3"
 
-    def update_timer(self):
-        while self.is_running:
-            print("remaining :", self.remaining_time)
-            self.remaining_time -= 1
+    def calc_temps_total(self):
+        if self.nombre_exercices == 1:
+            return (
+                self.temps_repos_series * (self.nombre_series - 1)
+                + self.temps_travail_par_exercice * self.nombre_series
+            )
+
+        temps_total_par_serie = (
+            self.temps_travail_par_exercice * self.nombre_exercices
+        ) + (self.temps_repos_exercices * (self.nombre_exercices - 1))
+
+        temps_total_toutes_series = (temps_total_par_serie * self.nombre_series) + (
+            self.temps_repos_series
+        ) * (self.nombre_series - 1)
+        return temps_total_toutes_series
+
+    # Function to update the timer
+    def update_timer(self, from_resume_exercice=False):
+        # Track the time spent working on an exercise
+        print(from_resume_exercice)
+        while self.is_running and self.remaining_time > 0:
+            print("remaining_time =", self.remaining_time)
+            print("current_serie =", self.current_serie)
+            print("current_exercise =", self.current_exercise)
+            print("worked_time =", self.worked_time)
+            print("exercise_break_countdown =", self.exercise_break_countdown)
+            print("serie_break_countdown =", self.serie_break_countdown)
+            print("is_in_exercise_break =", self.is_in_exercise_break)
+            print("is_in_serie_break =", self.is_in_serie_break)
+            print("=====================", "\n\n")
             time.sleep(1)
+            self.remaining_time -= 1
+            self.worked_time += 1
+
+            # those two conditions for when the user press on resume button
+            # added because if he was in break time and he toggle pause/resume then the programm will exit the function without finishing the break time (serie_break_countdown,serie_break_countdown)
+            # 7ydihom w fach tkoni f repos diri pause, tsnay wahd 5s, diri resume, aybalk lfer9
+            if self.is_in_exercise_break:
+                self.switch_to_exercise_break(True)
+                self.remaining_time += 1
+            if self.is_in_serie_break:
+                self.switch_to_series_break(True)
+                self.remaining_time += 1
+
+            # if he finish one exercise
+            if self.worked_time == self.temps_travail_par_exercice:
+                if self.current_exercise < self.nombre_exercices:
+                    self.switch_to_exercise_break()
+                else:
+                    self.switch_to_series_break()
+
+                self.worked_time = 0  # Reset the worked_time when switching between exercises or series
+
+            # If the remaining time reaches 0, stop the timer
             if self.remaining_time == 0:
-                # playsound(self.notification_sound)
                 self.is_running = False
+
+    # Function to switch to the exercise break period
+    def switch_to_exercise_break(self, from_resume_exercice=False):
+        if not from_resume_exercice:
+            self.current_exercise += 1
+            self.is_in_exercise_break = True
+
+        # Sleep for the duration of the exercise break time
+        for _ in range(self.exercise_break_countdown):
+            if not self.is_running:
+                break
+            time.sleep(1)
+            self.remaining_time -= 1
+            self.exercise_break_countdown -= 1
+
+        # to make sure he finished  his break before going back to work
+        if self.exercise_break_countdown == 0:
+            self.exercise_break_countdown = self.temps_repos_exercices
+            self.is_in_exercise_break = False
+
+    # Function to switch to the series break period
+    def switch_to_series_break(self, from_resume_exercice=False):
+        if not from_resume_exercice:
+            self.current_serie += 1
+            self.is_in_serie_break = True
+            self.current_exercise = 1
+
+        # Sleep for the duration of the series break time
+        for _ in range(self.serie_break_countdown):
+            if not self.is_running:
+                break
+            time.sleep(1)
+            self.remaining_time -= 1
+            self.serie_break_countdown -= 1
+
+        # to make sure he finished  his break before going back to work
+        if self.serie_break_countdown == 0:
+            self.serie_break_countdown = self.temps_repos_series
+            self.is_in_serie_break = False
 
 
 @app.route("/")
@@ -89,7 +185,16 @@ def stats():
 
 @app.route("/get_timer_info", methods=["POST"])
 def get_timer_info():
-    return jsonify(remaining_time=timer.remaining_time)
+    return jsonify(
+        remaining_time=timer.remaining_time,
+        current_exercise=timer.current_exercise,
+        current_serie=timer.current_serie,
+        is_in_exercise_break=timer.is_in_exercise_break,
+        is_in_serie_break=timer.is_in_serie_break,
+        serie_break_countdown=timer.serie_break_countdown,
+        exercise_break_countdown=timer.exercise_break_countdown,
+        worked_time=timer.worked_time,
+    )
 
 
 @app.route("/stop_resume_timer", methods=["POST"])
@@ -100,7 +205,9 @@ def stop_timer():
             return jsonify(paused=True)
         else:
             timer.is_running = True
-            timer.timer_thread = threading.Thread(target=timer.update_timer)
+            timer.timer_thread = threading.Thread(
+                target=timer.update_timer, args=[True]
+            )
             timer.timer_thread.start()
             return jsonify(paused=False)
 
--- static/js/stats.js
+++ static/js/stats.js
@@ -1,19 +1,19 @@
 // Accessing the values using data attributes
-// const nombre_series = document
-// 	.getElementById("nombre_series")
-// 	.getAttribute("data-value");
-// const nombre_exercices = document
-// 	.getElementById("nombre_exercices")
-// 	.getAttribute("data-value");
-// const temps_travail_par_exercice = document
-// 	.getElementById("temps_travail_par_exercice")
-// 	.getAttribute("data-value");
-// const temps_repos_exercices = document
-// 	.getElementById("temps_repos_exercices")
-// 	.getAttribute("data-value");
-// const temps_repos_series = document
-// 	.getElementById("temps_repos_series")
-// 	.getAttribute("data-value");
+const nombre_series = document
+  .getElementById("nombre_series")
+  .getAttribute("data-value");
+const nombre_exercices = document
+  .getElementById("nombre_exercices")
+  .getAttribute("data-value");
+const temps_travail_par_exercice = document
+  .getElementById("temps_travail_par_exercice")
+  .getAttribute("data-value");
+const temps_repos_exercices = document
+  .getElementById("temps_repos_exercices")
+  .getAttribute("data-value");
+const temps_repos_series = document
+  .getElementById("temps_repos_series")
+  .getAttribute("data-value");
 
 // console.log(
 // 	nombre_series,
@@ -23,58 +23,94 @@
 // 	temps_repos_series
 // );
 let intervalId;
+const temps_restant_span = document.getElementById("temps_restant");
+const nb_series_div = document.getElementById("nb_series");
+const nb_exercices_div = document.getElementById("nb_exercices");
+const nextEventLabel = document.getElementById("nextEventLabel");
+const work_pause_countdown = document.getElementById("work_pause_countdown");
 function updateTimerInfo() {
-	fetch("/get_timer_info", {
-		method: 'POST'
-	})
-		.then((response) => {
-			if (response.ok) {
-				return response.text();
-			}
-			throw new Error("Network response was not ok.");
-		})
-		.then((data) => {
-			const jsonData = JSON.parse(data); // Convert data to JSON
-			console.log(jsonData);
-			const temps_restant_span = document.getElementById("temps_restant");
-			temps_restant_span.innerText = jsonData.remaining_time;
-			if (jsonData.remaining_time <= 0) {
-				clearInterval(intervalId);
-			}
-		})
-		.catch(function (error) {
-			console.error("Error:", error);
-			if (intervalId) {
-				clearInterval(intervalId)
-			}
-		});
-}
+  fetch("/get_timer_info", {
+    method: "POST",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then((data) => {
+      const jsonData = JSON.parse(data); // Convert data to JSON
+      console.log(jsonData);
+      const {
+        current_exercise,
+        current_serie,
+        exercise_break_countdown,
+        is_in_exercise_break,
+        is_in_serie_break,
+        remaining_time,
+        serie_break_countdown,
+        worked_time,
+      } = jsonData;
+      temps_restant_span.innerText = remaining_time+"s";
 
+      if (!(is_in_exercise_break || is_in_serie_break)) {
+        nextEventLabel.innerText = `worked time: ${worked_time}s`;
+      }
+      if (is_in_exercise_break ) {
+        nextEventLabel.innerText = `next exercice in: ${exercise_break_countdown}s`;
+      }
+      if (is_in_serie_break) {
+        nextEventLabel.innerText = `next exercice in: ${serie_break_countdown}s`;
+      }
+      //for number of exercices
+      updateProgressBar(nb_exercices_div, current_exercise, nombre_exercices);
+
+      //for number of series
+      updateProgressBar(nb_series_div, current_serie, nombre_series);
+
+      if (remaining_time <= 0) {
+        clearInterval(intervalId);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    });
+}
+updateTimerInfo()
 intervalId = setInterval(updateTimerInfo, 1000);
-const togle_pause_btn = document.getElementById("pause_resume_btn")
-togle_pause_btn?.addEventListener('click', () => {
-	console.log("clicked");
-	fetch("/stop_resume_timer", {
-		method: "POST"
-	})
-		.then((response) => {
-			if (response.ok) {
-				return response.text();
-			}
-			throw new Error("Network response was not ok.");
-		})
-		.then((data) => {
-			const jsonData = JSON.parse(data); // Convert data to JSON
-			if (jsonData.paused) {
-				clearInterval(intervalId)
-				togle_pause_btn.innerText="Continue"
-			} else {
-				togle_pause_btn.innerText="Pause"
-				updateTimerInfo()
-				intervalId = setInterval(updateTimerInfo, 1000);
-			}
-		})
-		.catch(function (error) {
-			console.error("Error:", error);
-		});
-})
+const togle_pause_btn = document.getElementById("pause_resume_btn");
+togle_pause_btn?.addEventListener("click", () => {
+  console.log("clicked");
+  fetch("/stop_resume_timer", {
+    method: "POST",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then((data) => {
+      const jsonData = JSON.parse(data); // Convert data to JSON
+      if (jsonData.paused) {
+        clearInterval(intervalId);
+        togle_pause_btn.innerText = "Continue";
+      } else {
+        togle_pause_btn.innerText = "Pause";
+        updateTimerInfo();
+        intervalId = setInterval(updateTimerInfo, 1000);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+function updateProgressBar(divElement, currentData, totalData) {
+  divElement.classList.toggle("text-dark", currentData === 1);
+  divElement.innerText = `${currentData - 1}/${totalData}`;
+  divElement.style.width = `${(100 * (currentData - 1)) / totalData}%`;
+}
